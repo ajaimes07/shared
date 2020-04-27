9ad411479d692c7276fd71fdb9ebf748bcded4dd
Cindzzi <adeliatina.manuel@gmail.com>
2020-04-27 01:47:24 -0500

    heroku master

--- static/js/data.js
+++ static/js/data.js
@@ -1,12 +1,18 @@
 
-// Retrieve
-var MongoClient = require('mongodb').MongoClient;
+function buildPlot() {
+    /* data route */
+  var url = "mongodb://Aline1:aline1@ds061355.mlab.com:61355/heroku_njkl5bj0";
+  d3.json(url).then(function(response) {
 
-// Connect to the db
-MongoClient.connect("mongodb://Aline1:aline1@ds061355.mlab.com:61355/heroku_njkl5bj0", function(err, db) {
-  if(!err) {
-    console.log("We are connected");
+    console.log(response);
+
+    var data = response;
+  
+      Plotly.newPlot("plot", data, layout);
+    });
   }
-});
+buildPlot();
+  
 
-   
+
+
